3838dcb76a10dcfcda2047be316c0a1a890d2e47
Julian <Julian2016.wang@gmail.com>
2017-06-17 13:50:49 +1000

    添加项目文件。

---
+++ People/Controllers/PersonController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using People.Models;
+
+namespace People.Controllers
+{
+    public class PersonController : Controller
+    {
+        public static List<Person> people;
+
+        // GET: Person
+        public ActionResult ListPeople(int r = 0)
+        {
+            if (people == null)
+                people = InitPeople();
+
+            Races race = (Races)r;
+
+            List<Person> list = new List<Person>();
+            list = people.Where(p => (p.Race == race) && (p.Age % 2 == 0)).ToList();
+            list.Sort(SortPerson);
+
+            ViewBag.people = list;
+            ViewBag.race = r;
+            return View();
+        }
+
+        public JsonResult ListPeopleJson(int r = 0)
+        {
+            if (people == null)
+                people = InitPeople();
+
+            Races race = (Races)r;
+
+            List<Person> list = new List<Person>();
+            list = people.Where(p => (p.Race == race) && (p.Age % 2 == 0)).ToList();
+            list.Sort(SortPerson);
+
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetPeopleInformation()
+        {
+            int num = people.Count;
+            int min = 0, max = 0;
+            double average = 0;
+            int enumCount = Enum.GetValues(typeof(Races)).Length;
+
+            max = people.Max(p => p.Age);
+            min = people.Min(p => p.Age);
+            average = Math.Round(people.Average(p => p.Age), 2);
+
+            int[] numRace = new int[enumCount];
+            for (int i = 0; i < enumCount; i++)
+            {
+                numRace[i] = people.Count(p => p.Race == (Races)i);
+            }
+
+            string strJson = "{";
+            strJson += "'number':" + num.ToString() + ",";
+            strJson += "'age':{'average':" + average.ToString() + ", 'min':" + min.ToString() + ", 'max':" + max.ToString() + "}, ";
+            strJson += "'race':[";
+            for (int i = 0; i < enumCount; i++)
+            {
+                strJson += "'" + Enum.GetName(typeof(Races), (Races)i) + "':" + numRace[i].ToString();
+                if (i < enumCount - 1)
+                    strJson += ", ";
+            }
+            strJson += "]}";
+
+            return Json(strJson, JsonRequestBehavior.AllowGet);
+        }
+
+        private static int SortPerson(Person p1, Person p2)
+        {
+            //if (p1.Age.CompareTo(p2.Age) != 0)
+                return p1.Age.CompareTo(p2.Age);
+            //else
+            //    return p1.Name.CompareTo(p2.Name);
+        }
+
+        public static List<Person> InitPeople()
+        {
+            List<Person> people = new List<Person>();
+            Random rnd = new Random();
+
+            int enumCount = Enum.GetValues(typeof(Races)).Length;
+
+            for (int i = 0; i < 10000; i++)
+            {
+                Person p = new Person();
+                p.Name = "Person #" + i.ToString();
+                p.Age = rnd.Next(1, 99);
+                p.Race = (Races)rnd.Next(0, enumCount);
+                people.Add(p);
+            }
+
+            return people;
+        }
+
+        public static List<Person> AddAge(List<Person> people)
+        {
+            for (int i = 0; i < people.Count; i++)
+            {
+                people[i].Age += 1;
+            }
+            return people;
+        }
+    }
+}
---
+++ People/Models/Person.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace People.Models
+{
+    public enum Races { Angles, Saxons, Jutes, Asians}
+    public class Person
+    {
+        private int _Age;
+        private Races _Race;
+
+        public string Name { get; set; }
+        public int Age
+        {
+            get
+            {
+                return _Age;
+            }
+            set
+            {
+                _Age = value;
+                Height = GetHeight(_Age, _Race);
+            }
+        }
+        public Races Race
+        {
+            get
+            {
+                return _Race;
+            }
+            set
+            {
+                _Race = value;
+                Height = GetHeight(_Age, _Race);
+            }
+        }
+        public double Height { get; set; }
+
+        public override string ToString()
+        {
+            return "My name is '" + Name + "' and I am " + Age + " years old.";
+        }
+
+        private double GetHeight(int age, Races race)
+        {
+            double height;
+            switch (race)
+            {
+                case Races.Angles:
+                    height = 1.5 + age * 0.45;
+                    break;
+                case Races.Saxons:
+                    height = 1.5 + age * 0.45;
+                    break;
+                case Races.Jutes:
+                    height = age * 1.6 / 2;
+                    break;
+                case Races.Asians:
+                    height = 1.7 + (age + 2) * 0.23;
+                    break;
+                default:
+                    height = 0;
+                    break;
+            }
+            return Math.Round(height, 2);
+        }
+    }
+}
